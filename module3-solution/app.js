6ce3bc8793e57d88492cb026d852547a639a7358
skailas <smitha.gopalan26@gmail.com>
2016-12-29 04:21:06 -0700

    Test Module 3 Solution

---
+++ module3-solution/app.js
@@ -0,0 +1,121 @@
+(function() {
+    "use strict";
+
+    angular
+    .module("NarrowItDownApp", [])
+    .directive("foundItems", FoundItemsDirective)
+    .directive("itemsLoaderIndicator", ItemsLoaderIndicatorDirective)
+    .controller("FoundItemsDirectiveController", FoundItemsDirectiveController)
+    .controller("NarrowItDownController", NarrowItDownController)
+    .service("MenuSearchService", MenuSearchService)
+    .constant("restaurantApiBaseUrl", "https://davids-restaurant.herokuapp.com");
+
+
+    Array.prototype.any = function() {
+        return this.length >= 1;
+    }
+
+
+    function ItemsLoaderIndicatorDirective() {
+        return {
+            restrict: "E",
+            templateUrl: "items-loader-indicator.html"
+        };
+    }
+
+
+    function FoundItemsDirective() {
+        return {
+            restrict: "E",
+            templateUrl: "found-items.html",
+            scope: {
+                items: "<",
+                onRemove: "&"
+            },
+            controller: "FoundItemsDirectiveController as dirCtrl",
+            bindToController: true
+        };
+    }
+
+
+    function FoundItemsDirectiveController() {
+        var dirCtrl = this;
+    }
+
+
+    NarrowItDownController.$inject = ["MenuSearchService"];
+    function NarrowItDownController(menuSearchService) {
+        var narrowItDown = this;
+
+        narrowItDown.message = "";
+        narrowItDown.removeItem = removeItem;
+        narrowItDown.searchTerm = "";
+        narrowItDown.search = search;
+        narrowItDown.searchInProgress = false;
+        narrowItDown.shouldShowMessage = function() { return narrowItDown.message && !narrowItDown.searchInProgress; }; 
+        narrowItDown.found = null;
+
+        function removeItem(itemIndex) {
+            if (narrowItDown.found !== null) {
+                narrowItDown.found.splice(itemIndex, 1);
+                if (!narrowItDown.found.any()) {
+                    narrowItDown.message = "Nothing remaining.";
+                }
+            }
+        }
+
+        function search() {
+            if (narrowItDown.searchTerm.trim().length > 0)
+            {
+                narrowItDown.searchInProgress = true;
+                menuSearchService
+                .getMatchedMenuItems(narrowItDown.searchTerm)
+                .then(function(found) {
+                    narrowItDown.searchInProgress = false;
+                    narrowItDown.found = found;
+                    narrowItDown.message = narrowItDown.found.any() ? "" : "Nothing found.";
+                })
+                .catch(function(error) {
+                    narrowItDown.searchInProgress = false;
+                    narrowItDown.found = null;
+                    narrowItDown.message = error.message;
+                });
+            }
+            else
+            {
+                narrowItDown.found = null;
+                narrowItDown.message = "Nothing found.";
+            }
+        }
+    }
+
+
+    MenuSearchService.$inject = ["$http", "restaurantApiBaseUrl"];
+    function MenuSearchService($http, restaurantApiBaseUrl) {
+        var service = this;
+
+        service.getMatchedMenuItems = getMatchedMenuItems;
+
+        function getMatchedMenuItems(searchTerm) {
+
+            return $http({
+                method: "GET",
+                url: restaurantApiBaseUrl + "/menu_items.json"
+            })
+            .then(function(response) {
+                return response.data.menu_items.filter(containsIn("description", searchTerm));
+
+                function containsIn(property, subStr) {
+                    subStr = subStr.toLowerCase();
+                    return function(item) {
+                        return item[property].toLowerCase().indexOf(subStr) >= 0;
+                    };
+                }
+            })
+            .catch(function(errorResponse) {
+                throw new Error("Something went wrong when trying to fetch menu items.");
+            });
+        }
+    }
+
+})();
